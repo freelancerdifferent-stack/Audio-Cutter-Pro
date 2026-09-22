bae4b098a1bb6a0de51647a78b13694564dff3e4
Roman El Gibran <freelancerdifferent@gmail.com>
2026-09-22 12:06:10 +0700

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.differentfreelancer.audiocutterpro"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.differentfreelancer.audiocutterpro"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+}
